2a7ee2091cde1089833d1c76a33798560a20cb6b
Laurent De Schryver <laurentdeschryver@hotmail.com>
2018-08-18 15:53:06 +0200

    jenkins

--- karma.conf.js
+++ karma.conf.js
@@ -7,13 +7,23 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+     // require('karma-chrome-launcher'),
+     require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    files: [
+      { pattern: './src/test.ts', watched: false }
+    ],
+    preprocessors: {
+      './src/test.ts': ['@angular/cli']
+    },
+    mime: {
+      'text/x-typescript': ['ts', 'tsx']
     },
     coverageIstanbulReporter: {
       reports: [ 'html', 'lcovonly' ],
@@ -22,12 +32,16 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: config.angularCli && config.angularCli.codeCoverage
+    ? ['progress', 'coverage-istanbul']
+    :['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['PhantomJS'],
+    singleRun: true
   });
+
 };
+
